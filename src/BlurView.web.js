f2ab992fa8a06369ee6d48b8edf10e94b6405f83
Juan <justinjlf21@gmail.com>
2021-11-12 20:45:30 -0400

    ver 1.1.1

--- src/BlurView.web.js
+++ src/BlurView.web.js
@@ -5,9 +5,9 @@ import { StyleSheet } from 'react-native'
 
 
 const BlurView = (props)=>{
-
+   
      
-    const {blurNode,radius,style,className,...others} = props
+    const {blurNode,radius,style,className,children,...others} = props
      const r = radius === undefined ? 10 : radius
    
      const styleObject = useMemo(()=>{
@@ -27,15 +27,13 @@ const BlurView = (props)=>{
        }
        return style
      },[style])
-     
+
      const styleString = 
      `
            
             .blurjjlf {
               background-color: rgba(255, 255, 255, 0.95);
-              position: relative;
-              width: 100%;
-              height: 100%;
+
             }
             
             @supports (-webkit-backdrop-filter: none) or (backdrop-filter: none) {
@@ -48,15 +46,15 @@ const BlurView = (props)=>{
     `
 
     return(
-        <>
+        <div {...others} style={styleObject}>
           <style  type="text/css">
              {styleString}
           </style>
-           <div {...others}  className={`${className} blurjjlf`} style={styleObject}>
+           <div   className={`${className} blurjjlf`}  style={{width:'100%',height:'100%',position:'absolute',left:0,top:0}}>
                 
            </div>
-
-        </>
+          {children}
+        </div>
     )
 }
 
